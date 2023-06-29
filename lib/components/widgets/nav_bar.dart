03cca243949158954f804a3d839dfd2da9229f74
ukantjadia <ukantjadia0120@gmail.com>
2023-06-29 18:26:16 +0530

    data updated added the idea for the curve

--- lib/components/widgets/nav_bar.dart
+++ lib/components/widgets/nav_bar.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
-import 'package:proste_bezier_curve/proste_bezier_curve.dart';
 
 class navBar extends StatefulWidget {
   const navBar({super.key});
@@ -13,52 +10,71 @@ class navBar extends StatefulWidget {
 class _navBarState extends State<navBar> {
   @override
   Widget build(BuildContext context) {
-double screenWidth = MediaQuery. of(context). size. width;
+    double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body:Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: Container(child: Text('sf  dsfsfsfsf'),))
+              child: Container(
+            child: Text('sf  dsfsfsfsf'),
+          ))
         ],
       ),
       bottomNavigationBar: testContainer(),
-
-      
-    )
-    ;
+    );
   }
 }
 
 class testContainer extends StatefulWidget {
-  const testContainer( {super.key});
+  const testContainer({super.key});
 
   @override
   State<testContainer> createState() => _testContainerState();
 }
+
 class _testContainerState extends State<testContainer> {
   @override
-
   Widget build(BuildContext context) {
-    return Container(height:49,color: Colors.black45,
-    
-    child:  ClipPath(
-    clipper: ProsteBezierCurve(
-      position: ClipPosition.top,
-      list: [
-        BezierCurveSection(
-          start: Offset(30, 0),
-          top: Offset(60* 2, 0),
-          end: Offset(30, 0),
+    return Container(
+      height: 149,
+      color: Colors.black45,
+      child: ClipPath(
+        clipper:  WaveClipper(),
+        // ShapeBorderClipper(
+        //   shape: CircleBorder(
+        //     side: BorderSide(color: Colors.red, width: 4),
+        //   ),
+        // ),
+        child: Container(
+          color: Colors.red,
+          height: 150,
         ),
-      ],
-    ),
-    child: Container(
-      color: Colors.red,
-      height: 150,
-    ),
-  ),
+      ),
     );
   }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    double midwidth = size.width;
+    // double midheight = size.height;
+    path.lineTo(0, size.height);  // top 1st = a
+    path.lineTo(size.width,size.height);// bottom last = c
+    
+
+    path.lineTo(size.width,0);
+path.quadraticBezierTo(0, size.height/2, 0, 0);
+    // path.lineTo(0,size.height);
+    // path.lineTo(size.height,size.width);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
 }
--- lib/utils/cached_image.dart
+++ lib/utils/cached_image.dart
@@ -1,14 +1,14 @@
-import 'package:cached_network_image/cached_network_image.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
 
-// class helpers{
+// // class helpers{
+// //   void netImage(String url ){
+// //     CachedNetworkImage(imageUrl: url);
+// //   }
+
+// //   const helpers();
+// // }
+
 //   void netImage(String url ){
 //     CachedNetworkImage(imageUrl: url);
-//   }
-
-//   const helpers();
+// //   }
 // }
-
-  void netImage(String url ){
-    CachedNetworkImage(imageUrl: url);
-//   }
-}
